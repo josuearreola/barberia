43376a22d6e5a46743d84939b3a6042310419d5e
Josue <2023371053@uteq.edu.mx>
2026-03-23 22:48:53 -0600

    Remove unnecessary zone.js imports

--- frontend/src/main.server.ts
+++ frontend/src/main.server.ts
@@ -1,4 +1,3 @@
-import 'zone.js/node';
 import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
