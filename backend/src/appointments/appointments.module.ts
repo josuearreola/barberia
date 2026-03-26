0ed8f183f5cae2ac9b5fa18c231d5880bff8f2ec
Josue <2023371053@uteq.edu.mx>
2026-03-26 17:45:09 -0600

    Baja logicav2

--- backend/src/appointments/appointments.module.ts
+++ backend/src/appointments/appointments.module.ts
@@ -4,9 +4,10 @@ import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
 import { Appointment } from './entities/appointment.entity';
 import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Appointment]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Appointment]), AuthModule, UsersModule],
   controllers: [AppointmentsController],
   providers: [AppointmentsService],
   exports: [AppointmentsService],
